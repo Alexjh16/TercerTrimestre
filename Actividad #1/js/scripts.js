6691ed51c010b5240b9c7837969c97873d58d145
Alexjh16 <Alexx19@protonmail.com>
2019-07-27 11:57:27 -0500

    First Commit

---
+++ Actividad #1/js/scripts.js
@@ -0,0 +1,11 @@
+$("#empresas").click(function(){
+    window.location.href="services/empresas.html";
+});
+
+$("#empleos").click(function(){
+    window.location.href="services/empleos.html";
+});
+
+$("#empleados").click(function(){
+    window.location.href="services/empleados.html";
+});
